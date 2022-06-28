fb2d0e503c11b03a4820ba0fc91a4c2c4846b7ef
baixin <baixin.pan@mobvista.com>
2022-06-28 20:24:14 +0800

    fix arm64 error

--- Example/Pods/Target Support Files/BXSESDK/BXSESDK-umbrella.h
+++ Example/Pods/Target Support Files/BXSESDK/BXSESDK-umbrella.h
@@ -10,6 +10,8 @@
 #endif
 #endif
 
+#import "SEEventConstants.h"
+#import "SolarEngineSDK.h"
 
 FOUNDATION_EXPORT double BXSESDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char BXSESDKVersionString[];
